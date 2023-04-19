7d02fd0e44c518e405c9517f6bf11c305f4d6201
xiaoxuan <xiaoxuanpan0117@gmail.com>
2023-04-19 21:56:48 +0800

    feat: add animation to Banner

--- src/components/organisms/Banner/index.tsx
+++ src/components/organisms/Banner/index.tsx
@@ -1,4 +1,8 @@
-import * as React from "react";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 import { BannerWrapper, BannerContent, Title, Subtitle } from "./index.style";
 import Button from "../../atoms/Button";
 import Links from "../../molecules/Links";
@@ -6,9 +10,33 @@ import Links from "../../molecules/Links";
 export interface IBannerProps {}
 
 export default function Banner(props: IBannerProps) {
+  const bannerRef = useRef(null);
+  const bannerContentRef = useRef(null);
+
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.to(bannerContentRef.current, {
+        y: 50,
+        scrollTrigger: {
+          trigger: bannerRef.current,
+          scrub: 1,
+          start: "top top",
+          markers: {
+            startColor: "purple",
+            endColor: "fuchsia",
+            fontSize: "3rem",
+          },
+        },
+      });
+    });
+
+    return () => {
+      ctx.revert();
+    };
+  }, []);
   return (
-    <BannerWrapper>
-      <BannerContent>
+    <BannerWrapper ref={bannerRef}>
+      <BannerContent ref={bannerContentRef}>
         <Title>Xiao-xuan Pan</Title>
         <Subtitle>
           歡迎來到我的個人網站，這裡記錄了我的工作經驗與相關作品的介紹
