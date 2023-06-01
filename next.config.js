214b3b26968c64a883b69447dfd72b6fdcedd9af
xiaoxuan <xiaoxuanpan0117@gmail.com>
2023-06-01 22:59:13 +0800

    feat: add webpack analyzer

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 const { i18n } = require("./next-i18next.config");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   compiler: { styledComponents: true },
   i18n,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.plugins.push(new BundleAnalyzerPlugin());
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
